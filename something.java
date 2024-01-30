479743f85e5900c67734a52b28c5f8bc548a7550
gowtham2004 <spideysundar2004@gmail.com>
2024-01-30 13:40:06 +0530

    Chore: just an update

--- something.java
+++ something.java
@@ -1,9 +1,11 @@
 public class something {
     public static void main(String[] args){
-<<<<<<< HEAD
         System.out.println("This is something important from local");
+<<<<<<< Updated upstream
 =======
         System.out.println("This is something important from github");
 >>>>>>> 97bfbf728bc803bd21d15dcbef801ffaf8a262e7
+=======
+>>>>>>> Stashed changes
     }
 }
